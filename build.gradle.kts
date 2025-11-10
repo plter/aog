18ab0f7fd9a040247f0e871bc92f9dc7a198a2a3
https://yunp.top <opengit@yunp.top>
2025-11-11 06:10:24 +0800

    添加 jpackage 脚本

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
+import org.panteleyev.jpackage.ImageType
+
 plugins {
     kotlin("jvm") version "2.2.0"
+    id("application")
+    id("org.panteleyev.jpackageplugin") version "1.7.6"
 }
 
 group = "top.yunp"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -13,6 +17,20 @@ dependencies {
     implementation("org.graalvm.polyglot:polyglot:25.0.0")
     implementation("org.graalvm.polyglot:js:25.0.0")
     testImplementation(kotlin("test"))
+}
+
+application {
+    mainClass.set("Main")
+}
+
+tasks.jpackage {
+    dependsOn(tasks.clean, tasks.installDist)
+    input.set(projectDir.resolve("build").resolve("install").resolve(project.name).resolve("lib"))
+    mainClass.set(application.mainClass.get())
+    destination.set(projectDir.resolve("build").resolve("jpackage"))
+    appName.set(project.name)
+    mainJar.set("${project.name}-${project.version}.jar")
+    type.set(ImageType.APP_IMAGE)
 }
 
 tasks.test {
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ import org.graalvm.polyglot.Context;
 
 public class Main {
 
-
     public static void main(String[] args) {
         try (var context = Context.create()) {
             context.eval("js", "console.log('Hello from GraalJS!')");
